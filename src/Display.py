ac534af87896c5b011ab745dc33d5382f415ed8e
J132728 <J132728@tafe.edu.au>
2025-06-27 16:45:47 +0800

    added constructors and __str__ methods for classes

--- src/Display.py
+++ src/Display.py
@@ -1,2 +1,11 @@
 class Display:
-    pass
+    def __init__(self,
+                 display_id,
+                 message="",
+                 is_on=False):
+        self.id = display_id
+        self.message = message
+        self.is_on = is_on
+
+    def __str__(self):
+        return f"Display {self.id}: {self.message}"
